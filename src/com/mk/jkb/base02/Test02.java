889226207e7fcb3c1ab8fccc7131941d12726ed6
yyc <2344569@163.com>
2020-10-14 20:31:02 +0800

    no message

--- src/com/mk/jkb/base02/Test02.java
+++ src/com/mk/jkb/base02/Test02.java
@@ -20,21 +20,28 @@ public class Test02 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		  System.out.println("请输入数字：");
-		  Scanner scanner = new Scanner(System.in);
-		  int parm=scanner.nextInt();
-		  System.out.print(parm+"=");
-		  for(int i=2;i<=parm;i++){
-		    while(parm!=i){
-		      if(parm%i==0){ 
-	            System.out.print(i+"*");
-	            parm=parm/i;
-		        }else{
-	        	  break;
-		        }
-		    }
-		  }
-          System.out.print(parm);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入一个正整数:");
+		int n = sc.nextInt();
+		int k=2;
+		//输出第一步格式
+		System.out.print(n+"=");
+		//初值k为2,n为输入的数字,在程序执行的过程中k渐渐变大(k++),n渐渐变小(n/k)
+		while(k<=n){
+			//当n和k相等的时候,就直接输出n的值(此时输出k也行,因为n==k)
+			if(k==n){
+				System.out.println(n);
+				break;
+			} else if(n%k==0){
+				//如果n <> k，但n能被k整除，则应打印出k的值
+				System.out.print(k+"*");
+				//n除以k的商,作为新的正整数你n
+				n = n/k;
+			}else{
+				//如果n不能被k整除，则用k+1作为k的值
+				k++;
+			}
+		}
 	}
 
 }
